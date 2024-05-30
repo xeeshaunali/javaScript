68bd883edb1fcd21307875fff218f2eed81d343d
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2024-05-30 14:08:55 +0500

    JS

--- practice/app.js
+++ practice/app.js
@@ -189,24 +189,49 @@
 
 // document.write(string);
 
-var user = prompt("Enter Word").trim();
-var firstChar = user.slice(0,1).toUpperCase();
-console.log(firstChar); // check first Character is Working
-var otherChar = user.slice(1).toLowerCase();
-console.log(otherChar);
-var finalName = firstChar + otherChar;
-console.log(finalName);
+// var user = prompt("Enter Word").trim();
+// var firstChar = user.slice(0,1).toUpperCase();
+// console.log(firstChar); // check first Character is Working
+// var otherChar = user.slice(1).toLowerCase();
+// console.log(otherChar);
+// var finalName = firstChar + otherChar;
+// console.log(finalName);
 
-var names = ["Xee", "Shaun"];
+// var names = ["Xee", "Shaun"];
 
-for (var i = 0; i<=names.length; i++) {
-    if (finalName == names[i]){
-        console.log("Converted to Capital First Charactor");
+// for (var i = 0; i<=names.length; i++) {
+//     if (finalName == names[i]){
+//         console.log("Converted to Capital First Charactor");
+//         break;
+//     }
+//     else if (finalName !== names[i]) {
+//         console.log("Failed")
+//     }
+// }
+
+
+
+var str = prompt("Enter some text");
+
+var numChars = str.length;
+
+for (var i = 0; i < numChars; i++ ) {
+    if (str.slice(i, i + 2) === "  ") {
+        console.log("Double Spaces Found");
         break;
     }
-    else if (finalName !== names[i]) {
-        console.log("Failed")
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
