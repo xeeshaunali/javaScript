5793f5856db86c18ef137f1c8e467fb3a18c9d48
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2024-05-31 11:40:26 +0500

    JS

---
+++ 14) Check_IndexOF_String/Check_IndexOF_String.js
@@ -0,0 +1,9 @@
+var text = "A Quick Brown Fox";
+var num = text.indexOf("Fox");
+console.log(num);
+
+for (var i =0; i <=text.length; i++) {
+    var numChar = text.indexOf("Fox");
+    console.log(numChar);
+    break;
+}
--- practice/app.js
+++ practice/app.js
@@ -211,10 +211,15 @@
 
 
 
+var text = "A Quick Brown Fox";
+var num = text.indexOf("Fox");
+console.log(num);
 
-
-
-
+for (var i =0; i <=text.length; i++) {
+    var numChar = text.indexOf("Fox");
+    console.log(numChar);
+    break;
+}
 
 
 
