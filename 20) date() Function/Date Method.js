ee44d041beb08a496bd17d3dfd068702ccc6540a
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2024-06-04 12:26:23 +0500

    Date Method

--- 20) date() Function/Date Method.js
+++ 20) date() Function/Date Method.js
@@ -7,3 +7,19 @@ var string = date.toString();
 console.log(typeof(date));
 console.log(string);
 console.log(string.slice(0, 15))
+
+
+var tuesday = console.log(date.getDay());
+
+var May = console.log(date.getMonth());
+var dateOfMonth = console.log(date.getDate());
+
+var hours = console.log(date.getHours());
+
+console.log(date.getMinutes());
+
+console.log(date.getSeconds());
+
+console.log(date.getMilliseconds());
+
+console.log(date.getTime());
