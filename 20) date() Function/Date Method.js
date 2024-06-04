052c5d5380240bb342e4125352a7863bd343e99f
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2024-06-04 12:13:59 +0500

    Math Method

---
+++ 20) date() Function/Date Method.js
@@ -0,0 +1,9 @@
+// date function in JS
+var date = new Date();
+console.log(typeof(date));
+console.log(date.toString());
+console.log(typeof(date));
+var string = date.toString();
+console.log(typeof(date));
+console.log(string);
+console.log(string.slice(0, 15))
--- practice/app.js
+++ practice/app.js
@@ -225,8 +225,19 @@
 //     console.log("Tails Wins" + " " + tail);
 // }
 
-// toFixed() method fixes to range from decimal
-console.log(Math.random().toFixed(0));
-console.log(Math.random().toFixed(1));
-console.log(Math.random().toFixed(2));
-console.log(Math.random().toFixed(3));
+// // toFixed() method fixes to range from decimal
+// console.log(Math.random().toFixed(0));
+// console.log(Math.random().toFixed(1));
+// console.log(Math.random().toFixed(2));
+// console.log(Math.random().toFixed(3));
+
+
+
+var date = new Date();
+console.log(typeof(date));
+console.log(date.toString());
+console.log(typeof(date));
+var string = date.toString();
+console.log(typeof(date));
+console.log(string);
+console.log(string.slice(0, 15))
